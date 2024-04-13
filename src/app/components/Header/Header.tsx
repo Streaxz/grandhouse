080e5545f39f0ebdcdd54041d4f844af268cb365
lomonosov <lomonosov.200007@gmail.com>
2024-04-13 03:36:35 +0300

    link navbar to pages

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,8 +7,19 @@ import {MagnifierIcon} from "@/app/icons/MagnifierIcon";
 import {DialogBubbleIcon} from "@/app/icons/DialogBubbleIcon";
 import MenuIcon from '@mui/icons-material/Menu';
 import {useEffect} from "react";
+import Link from "next/link";
 
-const pages = ['Компания', 'Проекты', 'Услуги', 'Лес', 'Поселки', 'Работы и отзывы', 'Контакты', 'Журнал'];
+const pages = [
+    { name: 'Компания', link: '/company' },
+    { name: 'Проекты', link: '/catalog' },
+    { name: 'Услуги', link: '/offers' },
+    { name: 'Лес', link: '/forest' },
+    { name: 'Поселки', link: '/settlements' },
+    { name: 'Работы и отзывы', link: '/reviews' },
+    { name: 'Контакты', link: '/contacts' },
+    { name: 'Журнал', link: '/magazine' }
+];
+
 export const Header = () => {
     const isMobile = useMediaQuery('(max-width:500px)');
     const isTablet = useMediaQuery('(min-width:500px) and (max-width:1100px)');
@@ -28,12 +39,13 @@ export const Header = () => {
         }
 
 
-
-    },[isMobile, isTablet, isDesktop])
-    const renderMenuItems = (items: string[]) => {
+    }, [isMobile, isTablet, isDesktop])
+    const renderMenuItems = (items: { name: string; link: string }[]) => {
         return items.map((item, index) => (
           <Typography key={index} variant="h6" component="div" sx={{flexGrow: 1}}>
-              {item}
+              <Link key={index} href={item.link} style={{textDecoration: 'none'}}>
+                  {item.name}
+              </Link>
           </Typography>
         ));
     };
@@ -48,38 +60,40 @@ export const Header = () => {
     );
 
     return (
-      <div style={{          background: "rgba(255, 255, 255, 0.25)"}}>
-      <div style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          maxWidth: "1350px",
-          margin: "0 auto",
-          padding: `${padding}px`,
-          paddingBottom: `${padding}px`,
-      }}>
-          {(isMobile || isTablet) &&
-            <div className = {"iconContainer"}>
-                <MenuIcon/>
-            </div>
-          }
-          <div
-            style={{
-                flexGrow: 2,
-                display: "flex",
-                justifyContent: "center",
-            }}
-          >
-              <HeaderLogo/>
-          </div>
+      <div style={{background: "rgba(255, 255, 255, 0.25)"}}>
+          <div style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              maxWidth: "1350px",
+              margin: "0 auto",
+              padding: `${padding}px`,
+              paddingBottom: `${padding}px`,
+          }}>
+              {(isMobile || isTablet) &&
+                <div className={"iconContainer"}>
+                    <MenuIcon/>
+                </div>
+              }
+              <div
+                style={{
+                    flexGrow: 2,
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+              >
+                  <Link href={"/"}>
+                      <HeaderLogo/>
+                  </Link>
+              </div>
 
-          {isDesktop && renderMenuItems(pages)}
+              {isDesktop && renderMenuItems(pages)}
 
-          {isDesktop && renderIcon()}
-          <div className={'iconContainer'}>
-              <DialogBubbleIcon/>
+              {isDesktop && renderIcon()}
+              <div className={'iconContainer'}>
+                  <DialogBubbleIcon/>
+              </div>
           </div>
-      </div>
       </div>
     );
 }
